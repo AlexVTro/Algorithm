54c8e2896da551ff6e71daae31d49969c534fdc3
e92_git <5tr@mail.ru>
2014-10-26 19:44:17 +0700

    alg dll

---
+++ Algoritm 2/Slot/bin/Debug/Проекты/Примеры/Сложные проекты/Подключение Dll/Vb.net проект/WindowsApplication1/Main.vb
@@ -0,0 +1,6 @@
+﻿'Класс MainClass и функция Main должны обязательно присутствовать
+Public Class MainClass
+    Public Shared Sub Main()
+        Clipboard.SetText(Astro.jday(2014, 10, 10, 10, 10, 10))
+    End Sub
+End Class
